803761e3ebf888682ace6160f2170f68f32b3876
ahdaProject <husamsoma11@gmail.com>
2021-02-08 01:53:45 +0300

    ohda99

--- app/src/main/java/com/example/ohda499/Login.java
+++ app/src/main/java/com/example/ohda499/Login.java
@@ -1,14 +1,114 @@
 package com.example.ohda499;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 public class Login extends AppCompatActivity {
-
+    EditText mEmail, mPhone;
+        Button log;
+        TextView noaccount;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
     }
+    private boolean valiemail() {
+        String valu = mEmail.getEditableText().toString().trim();
+        String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
+        if (valu.isEmpty()) {
+            mEmail.setError("Email is required ! ");
+            return false;
+        } else if (!valu.matches(emailPattern)) {
+            mEmail.setError("Invalid email address ! ");
+            return false;
+
+        } else {
+            mEmail.setError(null);
+
+            return true;
+        }
+
+    }
+    private boolean validPass(){
+        String valu = mPhone.getEditableText().toString().trim();
+
+        if(valu.isEmpty()){
+            mPhone.setError("Password is required ! ");
+            return false;
+        }
+        else{
+            mPhone.setError(null);
+            return true;
+        }
+
+    }
+    public void Login (View view){
+        if (!validPass()|valiemail()){
+            return;
+        }else{
+           isUser();
+        }
+
+
+    }
+
+    private void isUser() {
+        String useremail = mEmail.getText().toString().trim();
+        String userpass = mPhone.getText().toString().trim();
+        DatabaseReference reference= FirebaseDatabase.getInstance().getReference("users");
+        Query check = reference.orderByChild("email").equalTo(userpass);
+        check.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists()){
+                    mEmail.setError(null);
+
+                    String passDB = snapshot.child(useremail).child("password").getValue(String.class);
+                    if(passDB.equals(userpass)){
+                        String nameDB = snapshot.child(useremail).child("fname").getValue(String.class);
+                        String emailDB = snapshot.child(useremail).child("email").getValue(String.class);
+                        String phoneDB = snapshot.child(useremail).child("phone").getValue(String.class);
+                        Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                        intent.putExtra("fname",nameDB);
+                        intent.putExtra("email",emailDB);
+                        intent.putExtra("phone",phoneDB);
+                        intent.putExtra("password",passDB);
+                        startActivity(intent);
+
+                    }
+                    else {
+                      mPhone.setError("Wrong Password");
+                      mPhone.requestFocus();
+                    }
+                }
+                else {
+                    mEmail.setError("no such User exist");
+                    mPhone.requestFocus();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+    }
+
 }
--- app/src/main/java/com/example/ohda499/regestr.java
+++ app/src/main/java/com/example/ohda499/regestr.java
@@ -22,23 +22,24 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class regestr extends AppCompatActivity {
- EditText mFullName, mEmail, mPassword, mPhone;
- Button mRegisterBtn, mlogin;
- FirebaseAuth fAuth;
- ProgressBar progressBar;
- FirebaseDatabase rootnode;
- DatabaseReference reference;
+    EditText mFullName, mEmail, mPassword, mPhone;
+    Button mRegisterBtn, mlogin;
+    FirebaseAuth fAuth;
+    ProgressBar progressBar;
+    FirebaseDatabase rootnode;
+    DatabaseReference reference;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_regestr);
 
+
         mFullName = findViewById(R.id.fullname);
-        mEmail  = findViewById(R.id.email);
-        mPassword= findViewById(R.id.password);
+        mEmail = findViewById(R.id.email);
+        mPassword = findViewById(R.id.password);
         mPhone = findViewById(R.id.phone);
         mRegisterBtn = findViewById(R.id.registerbutton);
-        mlogin =  findViewById(R.id.loginbtm);
 
 
         mRegisterBtn.setOnClickListener(new View.OnClickListener() {
@@ -46,20 +47,111 @@ public class regestr extends AppCompatActivity {
             public void onClick(View v) {
                 rootnode = FirebaseDatabase.getInstance();
                 reference = rootnode.getReference("users");
-
-
-                String fullname = mFullName.getEditableText().toString().trim();
-                String email = mEmail.getEditableText().toString().trim();
-                String password = mPassword.getEditableText().toString().trim();
-                String phone = mPhone.getEditableText().toString().trim();
-
-                helper help = new helper(fullname ,email,password,phone);
-
-                reference.child(password).setValue(help);
+                registerUser(v);
 
 
             }
         });
-        
+        mlogin = findViewById(R.id.loginbtm);
+
+        mlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(regestr.this,Login.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void registerUser(View view) {
+        if (!validname() | !valiemail() | !validPhone() | !validPass()) {
+            return;
+        }
+        String fullname = mFullName.getEditableText().toString().trim();
+        String email = mEmail.getEditableText().toString().trim();
+        String password = mPassword.getEditableText().toString().trim();
+        String phone = mPhone.getEditableText().toString().trim();
+
+        helper help = new helper(fullname, email, password, phone);
+
+        reference.child(password).setValue(help);
+
+    }
+
+    private boolean validname() {
+        String valu = mFullName.getEditableText().toString().trim();
+        if (valu.isEmpty()) {
+            mFullName.setError("Name is required ! ");
+            return false;
+        } else {
+            mFullName.setError(null);
+
+            return true;
+        }
+
+    }
+
+    private boolean valiemail() {
+        String valu = mEmail.getEditableText().toString().trim();
+        String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
+        if (valu.isEmpty()) {
+            mEmail.setError("Email is required ! ");
+            return false;
+        } else if (!valu.matches(emailPattern)) {
+            mEmail.setError("Invalid email address ! ");
+            return false;
+
+        } else {
+            mEmail.setError(null);
+
+            return true;
+        }
+
+    }
+
+    private boolean validPhone() {
+        String valu = mPassword.getEditableText().toString().trim();
+
+        if (valu.isEmpty()) {
+            mPassword.setError("Phone is required ! ");
+            return false;
+        } else if (mPassword.length() == 10) {
+            mPassword.setError("Phone must be 10 numbers ! ");
+            return false;
+        } else {
+
+            mPassword.setError(null);
+
+            return true;
+        }
+    }
+
+
+
+    private boolean validPass(){
+        String valu = mPhone.getEditableText().toString().trim();
+        String passvald= "^" +
+                //"(?=.*[0-9])" +         //at least 1 digit
+                //"(?=.*[a-z])" +         //at least 1 lower case letter
+                //"(?=.*[A-Z])" +         //at least 1 upper case letter
+                "(?=.*[a-zA-Z])" +      //any letter
+                "(?=.*[@#$%^&+=])" +    //at least 1 special character
+                "(?=\\S+$)" +           //no white spaces
+                ".{4,}" +               //at least 4 characters
+                "$";
+        if(valu.isEmpty()){
+            mPhone.setError("Password is required ! ");
+            return false;
+        }else if(!valu.matches(passvald)){
+            mPhone.setError("Password is weak !");
+           return false;
+
+        }
+        else{
+            mPhone.setError(null);
+            return true;
+        }
+
     }
 }
