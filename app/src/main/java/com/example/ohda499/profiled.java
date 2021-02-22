a96373b78a94ef33c01e6d580e4a947176b736ca
Havii <husamsoma10@gmail.com>
2021-02-23 01:19:31 +0300

    hii

--- app/src/main/java/com/example/ohda499/profiled.java
+++ app/src/main/java/com/example/ohda499/profiled.java
@@ -9,6 +9,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -24,7 +25,15 @@ public class profiled extends Fragment {
     Button mRegisterBtn, mlogin;
      TextView  hAcount;
     FirebaseDatabase rootnode;
-    DatabaseReference reference;
+
+    private Button mUpdate;
+
+    private EditText name,email, password ,phone;
+    String _name,_email, _password,_phone;
+    DatabaseReference Reference;
+
+
+
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -71,8 +80,148 @@ public class profiled extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_profiled, container, false);
+        Reference=FirebaseDatabase.getInstance().getReference("users");
+        System.out.println(Reference);
+        name= v.findViewById(R.id.NewName);
+        email=v.findViewById(R.id.NewEmail);
+        password =v.findViewById(R.id.NewPassward);
+        mUpdate =v.findViewById(R.id.Update);
+
+
+        showAllUserData();
+
+        mUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (registerUser(v)){
+                    update(v);
+                }
+
+
+            }
+        });
+
 
         return v;
     }
+    public boolean registerUser(View view) {
+        if (!validname() | !valiemail() | !validPass()) {
+            return false;
+        }
+
+    return true; }
+    private boolean valiemail() {
+        String valu = email.getEditableText().toString().trim();
+        String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
+        if (valu.isEmpty()) {
+            email.setError("Email is empty ! ");
+            return false;
+        } else if (!valu.matches(emailPattern)) {
+            email.setError("Invalid email address ! ");
+            return false;
+
+        } else {
+
+            email.setError(null);
+            return true;
+        }
+
+    }
+    private boolean validname() {
+        String valu = name.getEditableText().toString().trim();
+        if (valu.isEmpty()) {
+            name.setError("Name is Empty ! ");
+            return false;
+        } else {
+            name.setError(null);
+
+            return true;
+        }
+
+    }
+    private boolean validPass(){
+        String valu = password.getEditableText().toString().trim();
+        String passvald= "^" +
+                //"(?=.*[0-9])" +         //at least 1 digit
+                //"(?=.*[a-z])" +         //at least 1 lower case letter
+                //"(?=.*[A-Z])" +         //at least 1 upper case letter
+                "(?=.*[a-zA-Z])" +      //any letter
+                "(?=.*[@#$%^&+=])" +    //at least 1 special character
+                "(?=\\S+$)" +           //no white spaces
+                ".{4,}" +               //at least 4 characters
+                "$";
+        if(valu.isEmpty()){
+            password.setError("Password is required ! ");
+            return false;
+        }else if(!valu.matches(passvald)){
+            password.setError("Password is weak !");
+            return false;
+
+        }
+        else{
+            password.setError(null);
+            return true;
+        }
+
+    }
+    private void showAllUserData() {
+
+        Intent intent= getActivity().getIntent();
+        _name=intent.getStringExtra("fname");
+        _email=intent.getStringExtra("email");
+        _password =intent.getStringExtra("password");
+        _phone =intent.getStringExtra("phone");
+
+
+        System.out.println(_name);
+        System.out.println(_email);
+        System.out.println(_password);
+        System.out.println(_phone);
+
+
+        name.setText(_name);
+        email.setText(_email);
+        password.setText(_password);
+
+
+    }
+
+    public void update (View viwe){
+
+        if (isNameChanged() || isEmaillChanged() || isPasswordChanged()){
+
+            Toast.makeText(getActivity() , "Data has been updated", Toast.LENGTH_SHORT).show();
+        }else {
+            Toast.makeText(getActivity() , "There is no change in data", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+    private boolean isNameChanged() {
+        if (!_name.equals(name.getText().toString())) {
+            Reference.child(_phone).child("fname").setValue(name.getText().toString());
+            return true;
+        }else {
+            return false;
+        }
+    }
+    private boolean isEmaillChanged() {
+        if (!_email.equals(email.getText().toString())) {
+            Reference.child(_phone).child("email").setValue(email.getText().toString());
+            return true;
+        }else {
+            return false;
+        }
+    }   
+    private boolean isPasswordChanged() {
+        if (!_password.equals(password.getText().toString())) {
+            Reference.child(_phone).child("password").setValue(password.getText().toString());
+            return true;
+        }else {
+            return false;
+        }
+    }
+
 
 }
